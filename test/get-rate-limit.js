c5fa3b0bd8e36fd4fd709878b77603464abf6fbd
Paul Holden <pholden@stria.com>
2018-04-30 10:11:03 -0700

    Track request rate limit
    - Ideally handling rate limit responses and throttling requests to avoid
      hitting the limit should be done by the client, but this is really
      just groundwork for implementing that anyways.

---
+++ test/get-rate-limit.js
@@ -0,0 +1,41 @@
+const { describe, it, before, after } = require('mocha');
+const { expect } = require('chai');
+const OnePoint = require('../index');
+const env = require('./env.js');
+
+describe('run-report', function () {
+  var onePoint;
+
+  this.timeout(30000);
+
+  before(function (done) {
+    onePoint = new OnePoint(env);
+    onePoint.connect(done);
+  });
+
+  after(function (done) {
+    onePoint.close(done);
+  });
+
+  it('returns null for unknown rate limits', function (done) {
+    var onePoint = new OnePoint(env);
+
+    expect(onePoint.getRateLimit()).to.not.exist;
+
+    done();
+  });
+
+  it('returns current calls and threshold', function (done) {
+    onePoint.listReports((err, reports) => {
+      var limit = onePoint.getRateLimit();
+
+      expect(limit).to.exist;
+      expect(limit.calls).to.be.a('number');
+      expect(limit.calls).to.be.above(0);
+      expect(limit.threshold).to.be.a('number');
+      expect(limit.threshold).to.be.above(0);
+
+      done();
+    });
+  });
+});
